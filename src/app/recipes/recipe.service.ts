737fa76e9cef1220251573b3f4511c18fcb2ed01
prashant <prashant@arc.ai>
2025-01-13 12:55:38 +0530

    209 videos done, added method to add ingredients to the shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,40 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
     new Recipe(
       'Chicken Tikka Masala',
       'A popular Indian dish',
-      'https://www.licious.in/blog/wp-content/uploads/2020/12/Chicken-Tikka-Masala-min.jpg'
+      'https://www.licious.in/blog/wp-content/uploads/2020/12/Chicken-Tikka-Masala-min.jpg',
+      [
+        new Ingredient('Meet', 1),
+        new Ingredient('Tomato', 2),
+      ]
     ),
     new Recipe(
       'Chicken Fajitas',
       'A Mexican dish',
-      'https://www.tasteofhome.com/wp-content/uploads/2018/01/Flavorful-Chicken-Fajitas_EXPS_CIW19_12540_B08_30_6b-e1722260583441.jpg'
+      'https://www.tasteofhome.com/wp-content/uploads/2018/01/Flavorful-Chicken-Fajitas_EXPS_CIW19_12540_B08_30_6b-e1722260583441.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Pepper', 2),
+      ]
     ),
   ];
 
+  constructor(private shoppingListService: ShoppingListService){}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
