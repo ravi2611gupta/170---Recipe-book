3f5423b1e833eb82f64316dbe05d7aa10dc35005
prashant <prashantm.impinge@gmail.com>
2025-02-12 15:12:40 +0530

    417 vidoes done and optimizing the modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
-import { AuthComponent } from './auth/auth-component';
 
 const appRoute: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
- 
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+// import { RouterModule } from '@angular/router';
+// import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipesComponent } from './recipes.component';
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,14 +21,23 @@ import { RecipesRoutingModule } from './recipes-routing.module';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
+  //   exports: [
+  //     RecipesComponent,
+  //     RecipeListComponent,
+  //     RecipeItemComponent,
+  //     RecipeDetailComponent,
+  //     RecipeStartComponent,
+  //     RecipeEditComponent,
+  //   ],
+  // ! We don't need to export anymore after importing the RecipeRoutingModule. Because we used these component there. and these component are not being used in any other component except recipes.
+  // ! We no longer need to export the components after importing the RecipeRoutingModule because those components are only used within the Recipes component and not in any other components."
+
+  imports: [
+    // RouterModule,
+    // CommonModule,
+    SharedModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule,
   ],
-  imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
 export class RecipesModule {}
